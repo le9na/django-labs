94f7415bba4ba89707988b3be9309c3999fb7b96
Lena Abdulaziz <xlenaabdulaziz@gmail.com>
2024-10-24 05:42:29 +0300

    i

---
+++ CS471_Lab/apps/bookmodule/urls.py
@@ -0,0 +1,6 @@
+urlpatterns = [
+ path('', views.index, name= "books.index"),
+ path('list_books/', views.list_books, name= "books.list_books"),
+ path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+ path('aboutus/', views.aboutus, name="books.aboutus"),
+]
