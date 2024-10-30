da869ab830463168870a88e187e0f3d1a2ddf0c5
Lena Abdulaziz <xlenaabdulaziz@gmail.com>
2024-10-30 09:05:14 +0300

    Delete CS471_Lab directory

--- CS471_Lab/apps/bookmodule/urls.py
+++
@@ -1,6 +0,0 @@
-urlpatterns = [
- path('', views.index, name= "books.index"),
- path('list_books/', views.list_books, name= "books.list_books"),
- path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
- path('aboutus/', views.aboutus, name="books.aboutus"),
-]
